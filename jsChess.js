0791f248bc2db495bbc423045c5a04dee9ddd87b
Lucas Ausberger <74087830+lausberger@users.noreply.github.com>
2022-01-16 20:39:52 -0600

    added lots of possibilities for generation and iteration

--- jsChess.js
+++ jsChess.js
@@ -8,10 +8,43 @@ class GameBoard {
 		['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
 		['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
 		['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']]
+	
+	// could replace positions if we keep the Helper dicts
+	static altPositions = [
+		'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+		'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+		'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+		'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+		'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+		'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+		'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+		'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']
+		
+	// maybe these will be useful for iterating through many spaces at once?
+	static rankHelper = {
+		'a': ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8'],
+		'b': ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
+		'c': ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'],
+		'd': ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'],
+		'e': ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8'],
+		'f': ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8'],
+		'g': ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8'],
+		'h': ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']};
+
+	static fileHelper = {
+		1: ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
+		2: ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+		3: ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+		4: ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+		5: ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+		6: ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+		7: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+		8: ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8']};
 
 	constructor(dimension = 8) { // create an 8 by 8 chessboard
 		this.size = dimension
 		this.array = this.generateBoard()
+		this.spaces = this.generateBoard2()
 		this.pieces = this.generatePieces()
 		//this.updateStack = [] // could keep a list of rendering updates needed?
 		this.populateBoard()
@@ -19,7 +52,7 @@ class GameBoard {
 
 	generateBoard() {
 		var array = []
-		var switchColor = true
+		var switchColor = false
 		var rPtr = 0 
 		var fPtr = 0
 		for (var i = 0; i < this.size; i++) {
@@ -41,6 +74,24 @@ class GameBoard {
 		}
 
 		return array
+	}
+
+	// alternative way that lets us phase out array, as it is too hard to
+	// access specific Square objects by name
+	generateBoard2() {
+		var spaces = {}
+		var switchColor = false
+		for (var i in GameBoard.altPositions) {
+			let key = GameBoard.altPositions[i]
+			spaces[key] = new Square(
+				switchColor === true ? '#a3524e' : '#f2e8e7', 
+				key)
+			if (i % 7 != 0) {
+				switchColor = !switchColor
+			}
+		}
+		console.log(spaces)
+		return spaces
 	}
 
 	// perhaps populateBoard() needs to be removed, and each Piece here be
@@ -94,6 +145,7 @@ class GameBoard {
 		return pieces
 	}
 
+	// pretty much outmoded
 	populateBoard() { // hard coded for 8x8 board
 		// black pawns
 		for (var j = 0; j < this.size; j++) {
