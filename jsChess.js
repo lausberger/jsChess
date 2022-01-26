10c6c73833ca29d1449f33372f5091727fd0d50d
Lucas Ausberger <ausbergerl@hotmail.com>
2022-01-26 14:44:17 -0600

    implemented update function

--- jsChess.js
+++ jsChess.js
@@ -43,44 +43,13 @@ class GameBoard {
 
 	constructor(dimension = 8) { // create an 8 by 8 chessboard
 		this.size = dimension
-		//this.array = this.oldgenerateBoard()
 		this.spaces = this.generateBoard()
 		this.pieces = this.generatePieces()
-		//this.updateStack = [] // could keep a list of rendering updates needed?
+		// should this be a Stack for fast popping?
+		this.updateQueue = [] // could keep a list of rendering updates needed?
 		this.populateBoard()
 	}
 
-	/*
-	// should no longer be needed
-	oldgenerateBoard() {
-		var array = []
-		var switchColor = false
-		var rPtr = 0 
-		var fPtr = 0
-		for (var i = 0; i < this.size; i++) {
-			array.push([])
-			for (var j = 0; j < this.size; j++) {
-				let sq = new Square(
-					switchColor === true ? '#a3524e' : '#f2e8e7', 
-					GameBoard.positions[rPtr][fPtr])
-				array[i].push(sq)
-				if (fPtr == 7) {
-					fPtr = 0
-				} else {
-					fPtr += 1
-				}
-				switchColor = !switchColor
-			}
-			switchColor = !switchColor
-			rPtr += 1
-		}
-
-		return array
-	}
-	*/
-
-	// alternative way that lets us phase out array, as it is too hard to
-	// access specific Square objects by name
 	// #a3524e = burgundy
 	// #f2e8e7 = red-tinted white
 	generateBoard() {
@@ -96,51 +65,50 @@ class GameBoard {
 			}
 			switchColor = !switchColor
 		}
-		//console.log(spaces)
 		return spaces
 	}
 
 	generatePieces() {
 		var pieces = {}	
 		// black pawns
-		pieces['P1b'] = new Pawn('black', 'a7')
-		pieces['P2b'] = new Pawn('black', 'b7')
-		pieces['P3b'] = new Pawn('black', 'c7')
-		pieces['P4b'] = new Pawn('black', 'd7')
-		pieces['P5b'] = new Pawn('black', 'e7')
-		pieces['P6b'] = new Pawn('black', 'f7')
-		pieces['P7b'] = new Pawn('black', 'g7')
-		pieces['P8b'] = new Pawn('black', 'h7')
+		pieces['pb1'] = new Pawn('pb1', 'a7')
+		pieces['pb2'] = new Pawn('pb2', 'b7')
+		pieces['pb3'] = new Pawn('pb3', 'c7')
+		pieces['pb4'] = new Pawn('pb4', 'd7')
+		pieces['pb5'] = new Pawn('pb5', 'e7')
+		pieces['pb6'] = new Pawn('pb6', 'f7')
+		pieces['pb7'] = new Pawn('pb7', 'g7')
+		pieces['pb8'] = new Pawn('pb8', 'h7')
 
 		// black pieces
-		pieces['R8b'] = new Rook('black', 'a8')
-		pieces['R2b'] = new Rook('black', 'h8')
-		pieces['N8b'] = new Knight('black', 'b8')
-		pieces['N2b'] = new Knight('black', 'g8')
-		pieces['B8b'] = new Bishop('black', 'c8')
-		pieces['B2b'] = new Bishop('black', 'f8')
-		pieces['Qb'] = new Queen('black', 'd8')
-		pieces['Kb'] = new King('black', 'e8')
+		pieces['Rb1'] = new Rook('Rb1', 'a8')
+		pieces['Rb2'] = new Rook('Rb2', 'h8')
+		pieces['Nb1'] = new Knight('Nb1', 'b8')
+		pieces['Nb2'] = new Knight('Nb2', 'g8')
+		pieces['Bb1'] = new Bishop('Bb1', 'c8')
+		pieces['Bb2'] = new Bishop('Bb2', 'f8')
+		pieces['Qb'] = new Queen('Qb', 'd8')
+		pieces['Kb'] = new King('Kb', 'e8')
 		
 		// white pawns
-		pieces['P1w'] = new Pawn('white', 'a2')
-		pieces['P2w'] = new Pawn('white', 'b2')
-		pieces['P3w'] = new Pawn('white', 'c2')
-		pieces['P4w'] = new Pawn('white', 'd2')
-		pieces['P5w'] = new Pawn('white', 'e2')
-		pieces['P6w'] = new Pawn('white', 'f2')
-		pieces['P7w'] = new Pawn('white', 'g2')
-		pieces['P8w'] = new Pawn('white', 'h2')
+		pieces['pw1'] = new Pawn('pw1', 'a2')
+		pieces['pw2'] = new Pawn('pw2', 'b2')
+		pieces['pw3'] = new Pawn('pw3', 'c2')
+		pieces['pw4'] = new Pawn('pw4', 'd2')
+		pieces['pw5'] = new Pawn('pw5', 'e2')
+		pieces['pw6'] = new Pawn('pw6', 'f2')
+		pieces['pw7'] = new Pawn('pw7', 'g2')
+		pieces['pw8'] = new Pawn('pw8', 'h2')
 
 		// white pieces
-		pieces['R1w'] = new Rook('white', 'a1')
-		pieces['R2w'] = new Rook('white', 'h1')
-		pieces['N1w'] = new Knight('white', 'b1')
-		pieces['N2w'] = new Knight('white', 'g1')
-		pieces['B1w'] = new Bishop('white', 'c1')
-		pieces['B2w'] = new Bishop('white', 'f1')
-		pieces['Qw'] = new Queen('white', 'd1')
-		pieces['Kw'] = new King('white', 'e1')
+		pieces['Rw1'] = new Rook('Rw1', 'a1')
+		pieces['Rw2'] = new Rook('Rw2', 'h1')
+		pieces['Nw1'] = new Knight('Nw1', 'b1')
+		pieces['Nw2'] = new Knight('Nw2', 'g1')
+		pieces['Bw1'] = new Bishop('Bw1', 'c1')
+		pieces['Bw2'] = new Bishop('Bw2', 'f1')
+		pieces['Qw'] = new Queen('Qw', 'd1')
+		pieces['Kw'] = new King('Kw', 'e1')
 		
 		return pieces
 	}
@@ -150,34 +118,7 @@ class GameBoard {
 			let p = this.pieces[name]
 			this.spaces[p.pos].addPiece(p)
 		}
-		console.log(this.spaces)
 	}
-
-	/* 
-	// initial board rendering that used the array object
-	oldrenderBoard() {
-		var rBoard = document.getElementById('chessBoard')
-		for (var i in this.array) {
-			var rRow = document.createElement('div')
-			rRow.className = 'row'
-			rRow.id = i
-			for (var j in this.array[i]) {
-				var rSquare = document.createElement('div')
-				rSquare.className = 'square'
-				rSquare.id = this.array[i][j].id
-				rSquare.style.backgroundColor = this.array[i][j].color 
-				rRow.appendChild(rSquare)
-				if (this.array[i][j].contents != 'empty') {
-					var rPiece = document.createElement('img')
-					rPiece.className = 'piece'
-					rPiece.src = this.array[i][j].contents.img
-					rSquare.appendChild(rPiece)
-				}
-			}
-			rBoard.appendChild(rRow)
-		}
-	}
-	*/
 
 	// generate html elements from top to bottom
 	renderBoard() {
@@ -197,11 +138,7 @@ class GameBoard {
 					var rPiece = document.createElement('img')
 					rPiece.className = 'piece'
 					rPiece.src = this.spaces[id].contents.img
-					/*
-					we will need something like below to be able to alter
-					an rSquare's children after a move
-					*/
-					//rPiece.id = this.spaces[id].contents.id
+					rPiece.id = this.spaces[id].contents.id
 					rSquare.appendChild(rPiece)
 				}
 			}
@@ -210,7 +147,32 @@ class GameBoard {
 	}
 
 	updateBoard() {
-					
+		while (this.updateQueue.length) { // update queue is not empty
+			// not very efficient way of popping an element
+			let u = this.updateQueue.shift()
+
+			// string identifiers
+			let piece = u[1]
+			let src = u[2]
+			let dst = u[3]
+			
+			// HTML DOM objects
+			let rPiece = document.getElementById(piece)
+			let rSrc = document.getElementById(src)
+			let rDst = document.getElementById(dst)
+			console.log(u)
+			
+			// assume all updates are moves and are legal for now
+			if (rSrc.contains(rPiece) && !rDst.hasChildNodes()) {
+				this.spaces[src].contents = "empty"
+				this.spaces[dst].contents = this.pieces[piece]
+				this.pieces[piece].move(dst)
+				rSrc.removeChild(rPiece)
+				rDst.appendChild(rPiece)
+			} else {
+				console.log("something has gone horribly wrong")
+			}
+		}
 	}
 }
 
@@ -226,9 +188,9 @@ class Square {
 	}
 }
 
-// perhaps some kind of id is required? ex. 'R1w' 'Kb' 'N2w'
 class Piece {
-	constructor(position) {
+	constructor(id, position) {
+		this.id = id
 		this.pos = position
 	}
 
@@ -238,24 +200,22 @@ class Piece {
 }
 
 class Pawn extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['pawn moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/plt60.png'
-		} else {
+		} else { // assumes color is 'b'
 			this.img = 'pieces/pdt60.png'
 		}
 	}
 }
 
 class Rook extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['rook moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/rlt60.png'
 		} else {
 			this.img = 'pieces/rdt60.png'
@@ -264,11 +224,10 @@ class Rook extends Piece {
 }
 
 class Knight extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['knight moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/nlt60.png'
 		} else {
 			this.img = 'pieces/ndt60.png'
@@ -277,11 +236,10 @@ class Knight extends Piece {
 }
 
 class Bishop extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['bishop moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/blt60.png'
 		} else {
 			this.img = 'pieces/bdt60.png'
@@ -290,11 +248,10 @@ class Bishop extends Piece {
 }
 
 class Queen extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['queen moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/qlt60.png'
 		} else {
 			this.img = 'pieces/qdt60.png'
@@ -303,11 +260,10 @@ class Queen extends Piece {
 }
 
 class King extends Piece {
-	constructor(color, position) {
-		super(position)
-		this.color = color
+	constructor(id, position) {
+		super(id, position)
 		this.moveset = ['king moveset']
-		if (this.color == 'white') {
+		if (this.id.charAt(1) == 'w') {
 			this.img = 'pieces/klt60.png'
 		} else {
 			this.img = 'pieces/kdt60.png'
@@ -317,3 +273,16 @@ class King extends Piece {
 
 const chessBoard = new GameBoard()
 chessBoard.renderBoard()
+
+// Check Square 'e4'. Why does it have a pawn before updateBoard
+// has even been called?
+console.log(chessBoard.spaces)
+
+let testMove = ["move", "pw5", "e2", "e4"]
+chessBoard.updateQueue.push(testMove)
+
+setTimeout(function() {
+	chessBoard.updateBoard()
+	console.log(chessBoard.spaces)
+	console.log("These two dictionaries should not be the same at 'e4'")
+}, 3000);
